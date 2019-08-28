8b516d2b2c18a00f2d1cfc662c56d93ff84952a4
宋娟娟 <songjuanjuan@aeotrade.com>
2019-08-28 17:29:44 +0800

    project

--- myProject/src/view/home.vue
+++ myProject/src/view/home.vue
@@ -10,7 +10,7 @@
         <!-- 左 -->
         <el-col :span="6">
           <div style="background-color: blue">
-            <div id="module-left-top" :style="{height:contentHeight/7*3.8-2*12+'px',marginBottom:'0.8rem'}" style="background-color: chartreuse"
+            <div id="module-left-top" :style="{height:contentHeight/7*3.8+'px',marginBottom:'0.8rem'}" style="background-color: chartreuse"
                  class="stroke">
               <i class="inner-stroke-1"></i>
               <i class="inner-stroke-2"></i>
@@ -20,53 +20,56 @@
                 <span class="title-left">当月业务量</span>
               </p>
               <el-row>
-                <el-col :span="10" :offset="1">
-                  <div class="goods" style="background-color: black;" :style="{height:(contentHeight/7*3.8-2*12)/3*2-0.6*12+'px'}">
+                <el-col :span="11" :offset="1">
+                  <div class="goods" style="background-color: black;" :style="{height:(contentHeight/7*3.8)/3*2-1.6*12+'px'}">
                     <p>当月进口货物</p>
                     <countTo :startVal='500' :endVal='10287' :duration='5000' class="goods-number"></countTo>&nbsp;&nbsp;票
                     <div class="mounting">同比&nbsp;&nbsp;&nbsp;&nbsp;
                       <div v-show="isGoodsMounting" style="display: inline-block">
                         <span>12.5%</span>
                         <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#FFFF00"></path></svg>
-                      </div>
-                      <div v-show="!isGoodsMounting" style="display: inline-block">
-                        <span style="color: #58F1A3;">12.5%</span>
-                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#58F1A3"></path></svg>
+                        <svg v-show="!isGoodsMounting" t="1566980698715" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6608" width="12" height="12"><path d="M512.213359 958.989357 63.594899 510.930645l84.024695-84.023672L448.20351 729.129202 448.20351 63.546292l127.940904 0L576.144414 729.129202l299.651684-302.222229 84.009346 84.023672L512.213359 958.989357z" p-id="6609" fill="#FFFF00"></path></svg>
                       </div>
                     </div>
-                    <div class="mounting-chart" style="background-color: deeppink" :style="{height:(contentHeight/7*3.8-2*12)/3*2-9.7*12+'px'}"></div>
+                    <div class="mounting-chart" style="background-color: deeppink" :style="{height:(contentHeight/7*3.8)/3*2-9.7*12+'px'}"></div>
                   </div>
                 </el-col>
-                <el-col :span="10" :offset="2">
-                  <div class="goods" :style="{height:(contentHeight/7*3.8-2*12)/3*2-0.6*12+'px'}">
+                <el-col :span="11">
+                  <div class="goods" :style="{height:(contentHeight/7*3.8)/3*2-1.6*12+'px'}">
                     <p>当月提货车次</p>
                     <countTo :startVal='2000' :endVal='2027' :duration='2000' class="goods-number"></countTo>&nbsp;&nbsp;次
                     <div class="mounting">同比&nbsp;&nbsp;&nbsp;&nbsp;
-                      <div v-show="isCarMounting" style="display: inline-block">
-                        <span>12.5%</span>
-                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#FFFF00"></path></svg>
-                      </div>
-                      <div v-show="!isCarMounting" style="display: inline-block">
+                      <div style="display: inline-block">
                         <span style="color: #58F1A3;">12.5%</span>
-                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#58F1A3"></path></svg>
+                        <svg  v-show="isCarMounting" t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#58F1A3"></path></svg>
+                        <svg v-show="!isCarMounting" t="1566980698715" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6608" width="12" height="12"><path d="M512.213359 958.989357 63.594899 510.930645l84.024695-84.023672L448.20351 729.129202 448.20351 63.546292l127.940904 0L576.144414 729.129202l299.651684-302.222229 84.009346 84.023672L512.213359 958.989357z" p-id="6609" fill="#58F1A3"></path></svg>
                       </div>
                     </div>
-                    <div class="mounting-chart" style="background-color: deeppink" :style="{height:(contentHeight/7*3.8-2*12)/3*2-9.7*12+'px'}"></div>
+                    <div class="mounting-chart" style="background-color: deeppink" :style="{height:(contentHeight/7*3.8)/3*2-9.7*12+'px'}"></div>
                   </div>
                 </el-col>
                 <el-col :offset="1"></el-col>
               </el-row>
               <el-row>
-                <el-col :span="10" :offset="1">
-                  <div style="background-color: greenyellow;margin-top:1rem;" :style="{height:(contentHeight/7*3.8-2*12)/3-3*12+'px'}">1</div>
+                <el-col :span="11" :offset="1">
+                  <div class="tickets-number" style="background-color: greenyellow;" :style="{height:(contentHeight/7*3.8)/3-12+'px'}"><i class="blue-square"></i>当月退运票数
+                    <span class="blue">
+                      212票&nbsp;&nbsp;36%
+                    </span>
+                  </div>
                 </el-col>
-                <el-col :span="10" offset="2">
-                  <div style="background-color: greenyellow;margin-top:1rem;" :style="{height:(contentHeight/7*3.8-2*12)/3-3*12+'px'}">2</div>
+                <el-col :span="11">
+                  <div class="tickets-number" style="background-color: greenyellow;" :style="{height:(contentHeight/7*3.8)/3-12+'px'}"><i class="green-square"></i>当月查验票数
+                    <span class="green">
+                      789票&nbsp;&nbsp;99%
+                    </span>
+                  </div>
                 </el-col>
                 <el-col :offset="1"></el-col>
               </el-row>
             </div>
-            <div :style="{height:contentHeight/7*3.2-2*12-0.8*12+'px'}" style="background-color: coral"
+            <!-- 左下 -->
+            <div :style="{height:contentHeight/7*3.2-2.8*12+'px'}" style="background-color: coral"
                  class="col-container stroke">
               <i class="inner-stroke-1"></i>
               <i class="inner-stroke-2"></i>
@@ -80,12 +83,12 @@
         </el-col>
         <!-- 中 -->
         <el-col :span="12">
-          <div style="background-color: purple;" :style="{height:contentHeight-4*12+'px'}" class="center-content ">
+          <div style="background-color: purple;" :style="{height:contentHeight-2*12+'px'}" class="center-content ">
             <p class="module-title-center">
               <span class="title-center">模块1</span>
             </p>
             <div class="center-box">
-              <div :style="{height:contentHeight*3/4-4.5*12+'px'}" style="background-color: white" class="center-stroke">
+              <div :style="{height:contentHeight*3/4+'px'}" style="background-color: white" class="center-stroke">
                 <i class="inner-stroke-1"></i>
                 <i class="inner-stroke-2"></i>
                 <i class="inner-stroke-3"></i>
@@ -94,7 +97,7 @@
               <div style="background-color: coral" class="col-container">
                 <el-row>
                   <el-col :span="8" style="background-color: darkred">
-                    <div :style="{height:contentHeight-(contentHeight*3/4)-5.5*12+'px',marginRight:'1.2rem'}" class="polygon">
+                    <div :style="{height:contentHeight-(contentHeight*3/4)-8.5*12+'px',marginRight:'1.2rem'}" class="polygon">
                       <svg style="position: absolute;top:-0.5px;left: -1px" width="15" height="15" viewbox="0 0 100 200"
                            version="1.1" xmlns="http://www.w3.org/2000/svg">
                         <polyline points="15 0,10 0,0 10,0 15" style="fill:transparent;stroke:#00FFFF;stroke-width:2"/>
@@ -114,7 +117,7 @@
                     </div>
                   </el-col>
                   <el-col :span="8" style="background-color: chocolate">
-                    <div :style="{height:contentHeight-(contentHeight*3/4)-5.5*12+'px',marginRight:'0.6rem'}" class="polygon">
+                    <div :style="{height:contentHeight-(contentHeight*3/4)-8.5*12+'px',marginRight:'0.6rem'}" class="polygon">
                       <svg style="position: absolute;top:-0.5px;left: -1px" width="15" height="15" viewbox="0 0 100 200"
                            version="1.1" xmlns="http://www.w3.org/2000/svg">
                         <polyline points="15 0,10 0,0 10,0 15" style="fill:transparent;stroke:#00FFFF;stroke-width:2"/>
@@ -134,7 +137,7 @@
                     </div>
                   </el-col>
                   <el-col :span="8" style="background-color: darkred">
-                    <div :style="{height:contentHeight-(contentHeight*3/4)-5.5*12+'px',marginLeft:'0.6rem'}" class="polygon">
+                    <div :style="{height:contentHeight-(contentHeight*3/4)-8.5*12+'px',marginLeft:'0.6rem'}" class="polygon">
                       <svg style="position: absolute;top:-0.5px;left: -1px" width="15" height="15" viewbox="0 0 100 200"
                            version="1.1" xmlns="http://www.w3.org/2000/svg">
                         <polyline points="15 0,10 0,0 10,0 15" style="fill:transparent;stroke:#00FFFF;stroke-width:2"/>
@@ -266,7 +269,7 @@
 
   .container {
     background-color: green;
-    padding: 2rem 1rem;
+    padding: 1rem 0.5rem;
 
     .module-title-left {
       background: linear-gradient(left, rgba(16, 35, 137, .9), rgba(19, 56, 112, .2));
@@ -359,16 +362,14 @@
         border-right: 0.13rem solid #00FFFF;
       }
       .goods{
-        padding:0.5rem 0 0 0.5rem;
         box-sizing: border-box;
         font-family: sans-serif;
         text-align: left;
         color:#20FDFA;
-        /*border-bottom: 1px dotted rgba(31,45,92,.8);*/
         p{
           height: 3rem;
           line-height: 3rem;
-          font-size: 1.5rem;
+          font-size: 1.2rem;
           font-weight: 600;
           color: white;
         }
@@ -379,7 +380,7 @@
         .goods-number{
           font-weight: bold;
           font-family: Helvetica;
-          font-size: 3rem;
+          font-size: 2.5rem;
         }
         .mounting{
           color:#fff;
@@ -391,6 +392,30 @@
             color: #FFFF00;
             font-size: 1rem;
           }
+        }
+      }
+      .tickets-number{
+        font-family: Helvetica;
+        text-align: left;
+        color:white;
+        span.blue{
+          color:#2CFEFD;
+        }
+        i{
+          display: inline-block;
+          width: 0.9rem;
+          height: 0.9rem;
+          margin-right: 0.3rem;
+        }
+        i.blue-square{
+          background-color: #2CFEFD;
+        }
+        span.green{
+          color: #2AFD84;
+
+        }
+        i.green-square{
+          background-color: #2AFD84;
         }
       }
     }
