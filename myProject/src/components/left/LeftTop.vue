665c7f9a3a7e0c4ba0895b997217132f1d22f42c
宋娟娟 <songjuanjuan@aeotrade.com>
2019-08-28 16:17:23 +0800

    project vue数字滚动 前后端通信

--- myProject/src/components/left/LeftTop.vue
+++ myProject/src/components/left/LeftTop.vue
@@ -1,19 +1,20 @@
 <template>
-<div class="left-top"></div>
+  <div class="left-top">
+    <div class=""></div>
+  </div>
 </template>
 
 <script>
-export default {
-name: "LeftIndex",
-data(){
-    return{
-
-    }
-},
-methods:{
-
-}
-}
+  export default {
+    name: "LeftIndex",
+    props: ['lefttopwid', 'lefttophig'],
+    data() {
+      return {}
+    },
+    mounted() {
+    },
+    methods: {}
+  }
 </script>
 
 <style scoped lang="scss">
--- myProject/src/view/home.vue
+++ myProject/src/view/home.vue
@@ -10,15 +10,61 @@
         <!-- 左 -->
         <el-col :span="6">
           <div style="background-color: blue">
-            <div :style="{height:contentHeight/7*3.8-2*12+'px',marginBottom:'0.8rem'}" style="background-color: chartreuse"
+            <div id="module-left-top" :style="{height:contentHeight/7*3.8-2*12+'px',marginBottom:'0.8rem'}" style="background-color: chartreuse"
                  class="stroke">
               <i class="inner-stroke-1"></i>
               <i class="inner-stroke-2"></i>
               <i class="inner-stroke-3"></i>
               <i class="inner-stroke-4"></i>
               <p class="module-title-left">
-                <span class="title-left">模块1</span>
+                <span class="title-left">当月业务量</span>
               </p>
+              <el-row>
+                <el-col :span="10" :offset="1">
+                  <div class="goods" style="background-color: black;" :style="{height:(contentHeight/7*3.8-2*12)/3*2-0.6*12+'px'}">
+                    <p>当月进口货物</p>
+                    <countTo :startVal='500' :endVal='10287' :duration='5000' class="goods-number"></countTo>&nbsp;&nbsp;票
+                    <div class="mounting">同比&nbsp;&nbsp;&nbsp;&nbsp;
+                      <div v-show="isGoodsMounting" style="display: inline-block">
+                        <span>12.5%</span>
+                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#FFFF00"></path></svg>
+                      </div>
+                      <div v-show="!isGoodsMounting" style="display: inline-block">
+                        <span style="color: #58F1A3;">12.5%</span>
+                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#58F1A3"></path></svg>
+                      </div>
+                    </div>
+                    <div class="mounting-chart" style="background-color: deeppink" :style="{height:(contentHeight/7*3.8-2*12)/3*2-9.7*12+'px'}"></div>
+                  </div>
+                </el-col>
+                <el-col :span="10" :offset="2">
+                  <div class="goods" :style="{height:(contentHeight/7*3.8-2*12)/3*2-0.6*12+'px'}">
+                    <p>当月提货车次</p>
+                    <countTo :startVal='2000' :endVal='2027' :duration='2000' class="goods-number"></countTo>&nbsp;&nbsp;次
+                    <div class="mounting">同比&nbsp;&nbsp;&nbsp;&nbsp;
+                      <div v-show="isCarMounting" style="display: inline-block">
+                        <span>12.5%</span>
+                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#FFFF00"></path></svg>
+                      </div>
+                      <div v-show="!isCarMounting" style="display: inline-block">
+                        <span style="color: #58F1A3;">12.5%</span>
+                        <svg t="1566973708248" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5816" width="12" height="12"><path d="M959.804421 511.603981l-84.009346 84.023672L576.143391 293.406446l0 665.582911L448.20351 958.989357 448.20351 293.406446 147.619594 595.628676l-84.024695-84.023672L512.213359 63.546292 959.804421 511.603981z" p-id="5817" fill="#58F1A3"></path></svg>
+                      </div>
+                    </div>
+                    <div class="mounting-chart" style="background-color: deeppink" :style="{height:(contentHeight/7*3.8-2*12)/3*2-9.7*12+'px'}"></div>
+                  </div>
+                </el-col>
+                <el-col :offset="1"></el-col>
+              </el-row>
+              <el-row>
+                <el-col :span="10" :offset="1">
+                  <div style="background-color: greenyellow;margin-top:1rem;" :style="{height:(contentHeight/7*3.8-2*12)/3-3*12+'px'}">1</div>
+                </el-col>
+                <el-col :span="10" offset="2">
+                  <div style="background-color: greenyellow;margin-top:1rem;" :style="{height:(contentHeight/7*3.8-2*12)/3-3*12+'px'}">2</div>
+                </el-col>
+                <el-col :offset="1"></el-col>
+              </el-row>
             </div>
             <div :style="{height:contentHeight/7*3.2-2*12-0.8*12+'px'}" style="background-color: coral"
                  class="col-container stroke">
@@ -156,11 +202,15 @@
 
 <script>
   import Header from '@/components/common/Header.vue'
+  import LeftTop from '@/components/left/LeftTop.vue'
 
+  import countTo from 'vue-count-to'
   export default {
     name: "home",
     components: {
-      Header
+      Header,
+      LeftTop,
+      countTo
     },
     data() {
       return {
@@ -169,6 +219,9 @@
         contentHeight: 0,
         xwidth: 0,
         xheight: 0,
+        lefttopwid:0,
+        isGoodsMounting:true,// 同比增长样式
+        isCarMounting:false,
       }
     },
     methods: {
@@ -178,6 +231,7 @@
         this.contentHeight = document.documentElement.clientHeight - document.getElementById('header').offsetHeight;
         this.xwidth = document.getElementById('header').offsetWidth;
         this.xheight = document.getElementById('header').offsetHeight;
+        this.lefttopwid=document.getElementById('module-left-top').offsetWidth;
       },
     },
     computed: {},
@@ -303,6 +357,41 @@
         background-color: transparent;
         border-bottom: 0.13rem solid #00FFFF;
         border-right: 0.13rem solid #00FFFF;
+      }
+      .goods{
+        padding:0.5rem 0 0 0.5rem;
+        box-sizing: border-box;
+        font-family: sans-serif;
+        text-align: left;
+        color:#20FDFA;
+        /*border-bottom: 1px dotted rgba(31,45,92,.8);*/
+        p{
+          height: 3rem;
+          line-height: 3rem;
+          font-size: 1.5rem;
+          font-weight: 600;
+          color: white;
+        }
+        span{
+          font-weight: bold;
+          font-size: 1.5rem;
+        }
+        .goods-number{
+          font-weight: bold;
+          font-family: Helvetica;
+          font-size: 3rem;
+        }
+        .mounting{
+          color:#fff;
+          height: 2.5rem;
+          line-height: 2.5rem;
+          margin-top:0.5rem;
+          border-top: 1px dotted rgba(31,45,92,.8);
+          span{
+            color: #FFFF00;
+            font-size: 1rem;
+          }
+        }
       }
     }
 
